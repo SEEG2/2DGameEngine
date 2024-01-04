3c503fe74822113ed61eb3dd81e6038e451fa7f5
Sebastian <84249328+LegendPlay@users.noreply.github.com>
2024-01-04 09:48:57 +0100

    added resource management class

--- src/main/java/gmen/LevelEditorScene.java
+++ src/main/java/gmen/LevelEditorScene.java
@@ -4,6 +4,7 @@ package gmen;
 import components.SpriteRenderer;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
+import util.AssetPool;
 
 public class LevelEditorScene extends Scene {
 
@@ -31,8 +32,12 @@ public class LevelEditorScene extends Scene {
                 this.addGameObjectToScene(go);
             }
         }
+        loadResources();
     }
 
+    public void loadResources() {
+        AssetPool.getShader("assets/shaders/default.glsl");
+    }
     @Override
     public void update(float dt) {
         for (GameObject go : this.gameObjects) {
