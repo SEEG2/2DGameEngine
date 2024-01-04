c7b22b7aec4c573a5c23e01201b8810a5201a169
Sebastian <84249328+LegendPlay@users.noreply.github.com>
2024-01-04 09:52:48 +0100

    applies resource management

--- src/main/java/renderer/RenderBatch.java
+++ src/main/java/renderer/RenderBatch.java
@@ -3,6 +3,7 @@ package renderer;
 import components.SpriteRenderer;
 import gmen.Window;
 import org.joml.Vector4f;
+import util.AssetPool;
 
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
@@ -33,7 +34,7 @@ public class RenderBatch {
     private Shader shader;
 
     public RenderBatch(int maxBatchSize) {
-        shader = new Shader("assets/shaders/default.glsl");
+        shader = AssetPool.getShader("assets/shaders/default.glsl");
         shader.compile();
         this.sprites = new SpriteRenderer[maxBatchSize];
         this.maxBatchSize = maxBatchSize;
