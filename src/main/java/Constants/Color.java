0586a933de4a519a16a52465a52e2da5aff30ce4
Sebastian <84249328+LegendPlay@users.noreply.github.com>
2024-01-16 21:49:06 +0100

    added more colors

--- src/main/java/Constants/Color.java
+++ src/main/java/Constants/Color.java
@@ -12,8 +12,13 @@ public class Color {
     public static final Vector3f ORANGE = new Vector3f(1,0.5f,0);
     public static final Vector3f PINK = new Vector3f(1,0,1);
     public static final Vector3f GREY = new Vector3f(0.5f,0.5f,0.5f);
-    public static final Vector3f LIGHT_GREY = new Vector3f(0.75f,0.75f,0.75f);
     public static final Vector3f DARK_GREY = new Vector3f(0.25f,0.25f,0.25f);
+    public static final Vector3f LIGHT_GREY = new Vector3f(0.75f,0.75f,0.75f);
+    public static final Vector3f VERY_LIGHT_GREY = new Vector3f(0.85f,0.85f,0.85f);
+    public static final Vector3f VERY_DARK_GREY = new Vector3f(0.35f,0.35f,0.35f);
+    public static final Vector3f PURPLE = new Vector3f(0.6f,0.2f,1);
+    public static final Vector3f CYAN = new Vector3f(0,0.75f,0.9f);
+    public static final Vector3f BROWN = new Vector3f(0.6f,0.3f,0);
 
 
 }
