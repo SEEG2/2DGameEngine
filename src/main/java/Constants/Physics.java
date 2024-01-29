503c7f53c6c62041d5b40e2088234c6c4a09b74e
Sebastian <84249328+LegendPlay@users.noreply.github.com>
2024-01-29 19:34:18 +0100

    added physics class

---
+++ src/main/java/Constants/Physics.java
@@ -0,0 +1,5 @@
+package Constants;
+
+public class Physics {
+    public static final float GRAVITY = -9.81f;
+}
--- src/main/java/physics/Physics2D.java
+++ src/main/java/physics/Physics2D.java
@@ -1,11 +1,24 @@
 package physics;
 
 import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
+
+import static Constants.Physics.GRAVITY;
 
 public class Physics2D {
-    private Vec2 gravity = new Vec2(0, -9.8f);
+    private Vec2 gravity = new Vec2(0, GRAVITY);
+    private World world = new World(gravity);
+    private float physicsTime = 0;
+
+    private float physicsTimeStep = 1f/60;
+    private int velocityIterations = 12;
+    private int positionIterations = 5;
 
     public void update(float dt) {
-
+        physicsTime += dt;
+        if (physicsTime >= 0) {
+            physicsTime -= physicsTimeStep;
+            world.step(physicsTimeStep, velocityIterations, positionIterations);
+        }
     }
 }
